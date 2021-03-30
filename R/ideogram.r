e65fa32086ea8bdf0ca5210f320f11dd134295c6
dwinter <david.winter@gmail.com>
2021-03-30 16:15:14 +1300

    bare bones ideaogram code
    merging to main now, but keeping #2 open for as we improve this

--- R/ideogram.r
+++ R/ideogram.r
@@ -17,8 +17,10 @@ order_queries <- function(data){
 rm_ideogram <- function(rm_table){
     qs <- query_sizes(rm_table)
     rm_table$qname <- factor(rm_table$qname, levels=qs$qname)
-    ggplot() + geom_rect(data=qs, aes(xmin=0, xmax=qsize, ymax=-0.5, ymin=0.5), colour="black", fill="white") + facet_grid(qname ~.) + 
-            geom_rect(data=rm_table, aes(xmin=qstart, xmax=qend, ymax=-0.48, ymin=0.48))
+    ggplot() + geom_rect(data=qs, aes(xmin=0, xmax=qsize, ymax=-0.5, ymin=0.5), colour="black", fill="white") +
+        facet_grid(qname ~.) + 
+        geom_rect(data=rm_table, aes(xmin=qstart, xmax=qend, ymax=-0.48, ymin=0.48)) +
+        theme_ideogram()
 }
 
 theme_ideogram <- function() {
@@ -36,3 +38,26 @@ theme_ideogram <- function() {
     theme
 
 }
+
+#' Number formatters for scales in base pairs
+#'
+#' For use with \code{\link{ggplot2}} 
+#' @param x  The data (in base pairs) to be formatted as Gb, Mb or Kb
+#' @return  A character vector with scale labels
+#' @examples
+#' \dontrun{
+#' ali <- read_paf(system.file("extdata", "fungi.paf", package="pafr"))
+#' doplot(ali) + scale_x_continuous("Genomic position", label=Mb_lab)
+#'}
+#' @export
+#' @rdname Gb_lab
+Gb_lab <- function(x) paste(x / 1e9, "Gb")
+
+#' @export
+#' @rdname Gb_lab
+Mb_lab <- function(x) paste(x / 1e6, "Mb")
+
+#' @export
+#' @rdname Gb_lab
+Kb_lab <- function(x) paste(x / 1e3, "Kb")
+
